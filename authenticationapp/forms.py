a57a6e47b7f10d9fc7a422bcf9174998ef582e27
kalyanisankpal <sankpalkalyani93@gmail.com>
2024-06-13 20:29:43 +0530

    first commit

---
+++ authenticationapp/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+
+class CustomUserCreationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1')
+
+class CustomLoginForm(AuthenticationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control'}))
